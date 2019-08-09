760c7b78d72647b5e81fd84bd6f769e31bcd4603
vinnking <22546977@qq.com>
2019-08-09 17:37:53 +0800

    11

--- models/base.go
+++ models/base.go
@@ -1,1 +1,11 @@
 package models
+
+import (
+	"fmt"
+	"github.com/astaxie/beego"
+)
+
+// 返回带前缀的表名
+func TableName(str string) string {
+	return fmt.Sprintf("%s%s", beego.AppConfig.String("dbprefix"), str)
+}
